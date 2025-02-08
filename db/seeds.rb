f099b6aac1dafe10f78325c7088d24a26800c6ce
LayMui <mui_ume@hotmail.com>
2025-02-08 16:46:05 +0800

    add listing model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+require "open-uri"
+Listing.destroy_all
+
+listing = Listing.new(
+  name: 'Light & Spacious Garden Flat London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price: 75,
+  active: true,
+  capacity: 3
+)
+
+listing.photo.attach(io: File.open("#{Rails.root}/app/assets/images/san_francisco.jpg"), filename: 'san_francisco.jpg', content_type: 'image/jpg')
+listing.save
