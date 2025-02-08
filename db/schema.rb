be1636a29c0ac50550d214febbacfed9fbcf6579
amandasulistyo <amandaz.sulistyo@gmail.com>
2025-02-08 16:45:24 +0800

    bookings schema. listings controller. booking model. listing views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_08_073158) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_08_082743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "booking_reference"
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "confirmation_status"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_bookings_on_listing_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "name"
@@ -36,4 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_08_073158) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "listings"
+  add_foreign_key "bookings", "users"
 end
