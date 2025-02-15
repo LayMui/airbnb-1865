259d280ef9f5a3438f7452a459f4c2a7909d9e07
amandasulistyo <amandaz.sulistyo@gmail.com>
2025-02-15 16:48:20 +0800

    got the bookings new get parameters to get the proper parameters

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,4 +10,19 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @listing, notice: "Booking was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :start_date, :end_date, :number_of_guests)
+  end
 end
