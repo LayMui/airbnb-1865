677a382953671818bed857b394c43506c3749cfb
amandasulistyo <amandaz.sulistyo@gmail.com>
2025-02-15 11:54:46 +0800

    got the javascript to connect and work with yc's help

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,16 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr";
+import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
-// Connects to data-controller="datepicker"
 export default class extends Controller {
   static targets = ["dateInput", "startDate", "endDate"]
 
   connect() {
+    console.log("hello")
+    console.log(this.dateInputTarget)
+
     flatpickr(
       this.dateInputTarget,
       {
-        mode: "range",
-        onChange: (selectedDates, dateStr, instance) => {
+         mode: "range",
+         onChange: (selectedDates, dateStr, instance) => {
           this.startDateTarget.value = selectedDates[0].toISOString()
           if (selectedDates[1]) {
             this.endDateTarget.value = selectedDates[1].toISOString()
@@ -18,7 +20,7 @@ export default class extends Controller {
 
           console.log(this.startDateTarget.value)
           console.log(this.endDateTarget.value)
-        }
+         }
       }
     )
   }
